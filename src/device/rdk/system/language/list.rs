a94f43795443cbdb2f5dc5074b6b67cbdbfc3794
Denis Yuji Shimizu <denis.shimizu@collabora.com>
2023-02-03 10:58:40 -0300

    add system/language/list

--- src/device/rdk/system/language/list.rs
+++ src/device/rdk/system/language/list.rs
@@ -13,8 +13,7 @@ use crate::dab::system::language::list::GetAvailableLanguagesRequest;
 use crate::dab::system::language::list::GetAvailableLanguagesResponse;
 #[allow(unused_imports)]
 use crate::dab::ErrorResponse;
-use crate::device::rdk::interface::http_post;
-use serde::{Deserialize, Serialize};
+use crate::device::rdk::interface::get_rfc_5646_language_tag;
 use serde_json::json;
 
 #[allow(non_snake_case)]
@@ -24,48 +23,7 @@ pub fn process(_packet: String) -> Result<String, String> {
     let mut ResponseOperator = GetAvailableLanguagesResponse::default();
     // *** Fill in the fields of the struct GetAvailableLanguagesResponse here ***
 
-    #[derive(Serialize)]
-    struct RdkRequest {
-        jsonrpc: String,
-        id: i32,
-        method: String,
-        params: String,
-    }
-
-    let request = RdkRequest {
-        jsonrpc: "2.0".into(),
-        id: 3,
-        method: "org.rdk.DisplaySettings.getConnectedVideoDisplays".into(),
-        params: "{}".into(),
-    };
-
-    #[derive(Deserialize)]
-    struct RdkResponse {
-        jsonrpc: String,
-        id: i32,
-        result: GetConnectedVideoDisplaysResult,
-    }
-
-    #[derive(Deserialize)]
-    struct GetConnectedVideoDisplaysResult {
-        connectedVideoDisplays: Vec<String>,
-        success: bool,
-    }
-
-    let json_string = serde_json::to_string(&request).unwrap();
-    let response_json = http_post(json_string);
-
-    match response_json {
-        Ok(val2) => {
-            let rdkresponse: RdkResponse = serde_json::from_str(&val2).unwrap();
-        }
-
-        Err(err) => {
-            println!("Erro: {}", err);
-
-            return Err(err);
-        }
-    }
+    ResponseOperator.languages = get_rfc_5646_language_tag();
 
     // *******************************************************************
     let mut ResponseOperator_json = json!(ResponseOperator);
