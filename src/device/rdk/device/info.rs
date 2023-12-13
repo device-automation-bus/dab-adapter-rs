f4a4a6b58750eaaf040cbdae8e5047e1795bcde5
Jakub Adam <jakub.adam@collabora.com>
2023-12-13 19:12:47 +0000

    device/info: fix "uptimeSince" response parameter
    The parameter should contain unix timestamp of when the device was last
    booted. RDK gives us seconds since last boot, so we need to convert the
    value.

--- src/device/rdk/device/info.rs
+++ src/device/rdk/device/info.rs
@@ -43,6 +43,7 @@
 // pub uptimeSince: u64,
 // }
 
+use std::time::{SystemTime, UNIX_EPOCH};
 #[allow(unused_imports)]
 use crate::dab::structs::DeviceInfoRequest;
 use crate::dab::structs::DisplayType;
@@ -434,8 +435,14 @@ pub fn process(_packet: String) -> Result<String, String> {
         }
         ResponseOperator.networkInterfaces.push(interface);
     }
+
+    let ms_since_epoch = (SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .map_err(|err| ErrorResponse { status: 500, error: err.to_string() })?
+        .as_secs() - Systeminfo.result.uptime) * 1000;
+
     ResponseOperator.serialNumber = Systeminfo.result.serialnumber;
-    ResponseOperator.uptimeSince = Systeminfo.result.uptime;
+    ResponseOperator.uptimeSince = ms_since_epoch;
     ResponseOperator.manufacturer = DeviceInfo.result.make;
     ResponseOperator.firmwareVersion = DeviceInfo.result.imageRevision;
     ResponseOperator.firmwareBuild = DeviceInfo.result.imageVersion;
