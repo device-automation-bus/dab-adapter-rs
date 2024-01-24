9452452566b8296aa88aa1a6f5a0eceafd8ce72c
Denis Shimizu <denis.shimizu@collabora.com>
2024-01-24 19:08:40 -0300

    voice: Add websocket functions and routine to wait onSessionEnd event

--- src/device/rdk/interface.rs
+++ src/device/rdk/interface.rs
@@ -1,13 +1,23 @@
 use crate::dab::structs::AudioOutputMode;
 use crate::dab::structs::DabError;
 use futures::executor::block_on;
+use futures_util::stream::StreamExt;
+use futures_util::SinkExt;
 use lazy_static::lazy_static;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde_json::Value;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 use std::io::Write;
 use surf::Client;
+use tokio::net::TcpStream;
+use tokio::time::{timeout, Duration};
+use tokio_tungstenite::{
+    connect_async, tungstenite::protocol::Message, MaybeTlsStream, WebSocketStream,
+};
+use url::Url;
+
 static mut DEVICE_ADDRESS: String = String::new();
 static mut DEBUG: bool = false;
 
@@ -90,6 +100,57 @@ pub fn http_post(json_string: String) -> Result<String, DabError> {
 
             return Err(DabError::Err500(str));
         }
+    }
+}
+
+pub async fn ws_open() -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>, DabError> {
+    let rdk_address = format!("ws://{}:9998/jsonrpc", unsafe { &DEVICE_ADDRESS });
+    let url = Url::parse(&rdk_address).expect("Invalid WebSocket URL");
+
+    connect_async(url)
+        .await
+        .map_err(|e| DabError::Err500(format!("Failed to connect: {}", e)))
+        .map(|(ws_stream, _)| ws_stream)
+}
+
+pub async fn ws_close(
+    ws_stream: &mut WebSocketStream<MaybeTlsStream<TcpStream>>,
+) -> Result<(), DabError> {
+    ws_stream
+        .close(None)
+        .await
+        .map_err(|e| DabError::Err500(format!("Failed to close WebSocket: {}", e)))
+}
+
+pub async fn ws_send(
+    ws_stream: &mut WebSocketStream<MaybeTlsStream<TcpStream>>,
+    payload: Value,
+) -> Result<(), DabError> {
+    ws_stream
+        .send(Message::Text(payload.to_string()))
+        .await
+        .map_err(|e| DabError::Err500(format!("Failed to send message: {}", e)))
+}
+
+pub async fn ws_receive(
+    ws_stream: &mut WebSocketStream<MaybeTlsStream<TcpStream>>,
+) -> Result<Value, DabError> {
+    match timeout(Duration::from_secs(10), ws_stream.next()).await {
+        Ok(Some(Ok(message))) => {
+            if let Message::Text(text) = message {
+                serde_json::from_str(&text)
+                    .map_err(|e| DabError::Err500(format!("Invalid JSON: {}", e)))
+            } else {
+                Err(DabError::Err500("Received a non-text message".to_string()))
+            }
+        }
+        Ok(Some(Err(e))) => Err(DabError::Err500(format!("Error reading message: {:?}", e))),
+        Ok(None) => Err(DabError::Err500(
+            "The WebSocket stream has been closed by the server".to_string(),
+        )),
+        Err(_) => Err(DabError::Err500(
+            "Timeout occurred while waiting for a response".to_string(),
+        )),
     }
 }
 
