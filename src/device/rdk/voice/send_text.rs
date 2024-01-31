be460174cca4d68f740abcf9ee9f716dab3b7088
Denis Shimizu <denis.shimizu@collabora.com>
2024-01-31 18:25:46 -0300

    voice:send-text: remove crate dependency for tts, download directly from Google Translate. This is what the tts_rust only does, after all.This will avoid native library dependencies and help cross building

--- src/device/rdk/voice/send_text.rs
+++ src/device/rdk/voice/send_text.rs
@@ -1,27 +1,41 @@
 use super::voice_functions::sendVoiceCommand;
 use crate::dab::structs::DabError;
 use crate::dab::structs::SendTextRequest;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::process::Command;
+use surf::Url;
+use tokio::runtime::Runtime;
+use urlencoding::encode;
 
 #[allow(non_snake_case)]
 #[allow(dead_code)]
 #[allow(unused_mut)]
 pub fn process(_dab_request: SendTextRequest) -> Result<String, DabError> {
-    use std::process::Command;
-    use tts_rust::{languages::Languages, tts::GTTSClient};
-
     // TODO: Add other RDK specific voice protocol support confirmation.
     if _dab_request.voiceSystem.to_string() != "AmazonAlexa" {
         return Err(DabError::Err400("Unsupported 'voiceSystem'.".to_string()));
     }
 
-    let narrator: GTTSClient = GTTSClient {
-        volume: 1.0,
-        language: Languages::English,
-        tld: "com",
-    };
-    narrator
-        .save_to_file(&_dab_request.requestText, "/tmp/tts.mp3")
-        .expect("Failed to save to file");
+    // Use Google Translate TTS to generate the voice file.
+    let len = _dab_request.requestText.len();
+    let encoded_text = encode(&_dab_request.requestText).into_owned();
+    let mp3_file_path = Path::new("/tmp/tts.mp3");
+    let url = format!("https://translate.google.com/translate_tts?ie=UTF-8&q={}&tl=en&total=1&idx=0&textlen={}&tl=en&client=tw-ob",&encoded_text,len);
+
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        let parsed_url = Url::parse(&url).map_err(|e| e.to_string())?;
+        let mut res = surf::get(parsed_url).await.map_err(|e| e.to_string())?;
+        let body = res.body_bytes().await.map_err(|e| e.to_string())?;
+        let mut file = File::create(mp3_file_path).map_err(|e| e.to_string())?;
+        file.write_all(&body).map_err(|e| e.to_string())?;
+        Ok(())
+    })
+    .map_err(|e| DabError::Err500(e))?;
+
+    // Convert the mp3 file to wav.
 
     let mut child = Command::new("gst-launch-1.0")
         .arg("-q")
